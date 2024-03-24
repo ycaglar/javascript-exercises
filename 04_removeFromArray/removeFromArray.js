100499d7c6564a8defa7836ddd6d564296bbbfdf
Çağlar <2038887+ycaglar@users.noreply.github.com>
2024-03-24 13:13:26 -0500

    Complete exercise #4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,5 @@
-const removeFromArray = function() {
-
+const removeFromArray = function (arr, ...vals) {
+  return arr.filter(arrElem => !vals.includes(arrElem));
 };
 
 // Do not edit below this line
