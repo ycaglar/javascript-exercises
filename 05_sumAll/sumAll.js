ffbef9bd3df451c69af2479f2fccacf6238bc5de
Çağlar <2038887+ycaglar@users.noreply.github.com>
2024-03-24 13:26:29 -0500

    Complete exercise #5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,11 @@
-const sumAll = function() {
+const sumAll = function (m, n) {
+  if (m < 0 || n < 0 || typeof m !== 'number' || typeof n !== 'number') {
+    return 'ERROR';
+  };
+  leftBoundary = Math.min(m, n);
+  rightBoundary = Math.max(m, n);
 
+  return (rightBoundary + leftBoundary) * (rightBoundary - leftBoundary + 1) / 2
 };
 
 // Do not edit below this line
