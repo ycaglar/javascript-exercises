503ab2df37ba9b0165e6d47a6ad97d0f658178a4
Çağlar <2038887+ycaglar@users.noreply.github.com>
2024-03-24 12:41:30 -0500

    Complete exercise #2

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,8 @@
-const repeatString = function() {
-
+const repeatString = function (string, repcount) {
+  if (repcount < 0) {
+    return 'ERROR';
+  };
+  return string.repeat(repcount);
 };
 
 // Do not edit below this line
