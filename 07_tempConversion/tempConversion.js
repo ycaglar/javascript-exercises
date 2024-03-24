54c0311caf0cf7a66b6e54d36f946331f6518868
Çağlar <2038887+ycaglar@users.noreply.github.com>
2024-03-24 13:55:12 -0500

    Complete exercise #7

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function (temp) {
+  // °C = (°F - 32) × 5/9
+  return parseFloat(((temp - 32) * 5 / 9).toFixed(1));
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function (temp) {
+  // °F = (9/5 × °C) + 32
+  return parseFloat((9 / 5 * temp + 32).toFixed(1))
 };
 
 // Do not edit below this line
